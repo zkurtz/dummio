893f27ab2f85f08ec0cb12a34c0c953a54ff9b0e
Zach Kurtz <zkurtz@protonmail.com>
2025-02-13 18:01:10 -0500

    io for pandas series

--- dummio/pandas/__init__.py
+++ dummio/pandas/__init__.py
@@ -1,5 +1,6 @@
 from dummio.pandas import df_csv as df_csv
 from dummio.pandas import df_parquet as df_parquet
+from dummio.pandas import series_parquet as series_parquet
 
 try:
     from dummio.pandas import df_feather as df_feather
